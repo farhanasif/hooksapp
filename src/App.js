d4c0fb6810ec609e85d0bffa46b334a5007ad6e5
Farhan Asif <f.asif2010@gmail.com>
2020-05-26 15:52:20 +0600

    context setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
